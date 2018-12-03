97741afa2f289a941081461c4b278889bb9d8946
igrekus <upload234@mail.ru>
2018-12-03 23:33:30 +0000

    feat: add group filter to consultant and developer field

--- dc_plc/dc_plc/doctype/dc_plc_product_summary/dc_plc_product_summary.js
+++ dc_plc/dc_plc/doctype/dc_plc_product_summary/dc_plc_product_summary.js
@@ -2,7 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('DC_PLC_Product_Summary', {
-	refresh: function(frm) {
-
-	}
+    refresh: function(frm) {
+        cur_frm.fields_dict['tab_consultants'].grid.get_field('link_employee').get_query = function(doc, cut, cdn) {
+            return {
+                query: 'dc_plc.controllers.queries.consultant_query'
+            }
+        };
+        cur_frm.fields_dict['tab_developers'].grid.get_field('link_employee').get_query = function(doc, cut, cdn) {
+            return {
+                query: 'dc_plc.controllers.queries.developer_query'
+            }
+        }
+    }
 });
+
+// frappe.ui.form.on("Purchase Order", "refresh", function(frm) {
+//     cur_frm.set_query("supplier", function() {
+//         return {
+//             "filters": {
+//                 "currency_pay_in": "USD"
+//             }
+//         };
+//     });
+// });
+
