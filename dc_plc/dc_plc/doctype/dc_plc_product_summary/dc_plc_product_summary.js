cb858d900a472343c3ae2fe4ebc3f51cca5d182a
igrekus <upload234@mail.ru>
2018-12-04 09:19:09 +0000

    research: DDL behavior research

--- dc_plc/dc_plc/doctype/dc_plc_product_summary/dc_plc_product_summary.js
+++ dc_plc/dc_plc/doctype/dc_plc_product_summary/dc_plc_product_summary.js
@@ -13,8 +13,24 @@ frappe.ui.form.on('DC_PLC_Product_Summary', {
                 query: 'dc_plc.controllers.queries.developer_query'
             }
         }
+    },
+    // fire an even on property update
+    link_package: frm => {
+        // console.log(frm.fields_dict['link_package'].$input);
+        let value = frm.fields_dict['link_package'].value;
+        let list = frm.fields_dict['link_package'].awesomplete._list;
+        if (typeof value !== 'undefined') {
+            list.forEach( ( {description, val, label} ) => {
+                if (value === label) {
+                    console.log('found package:', label, description);
+                }
+            });
+        }
     }
 });
+
+
+
 
 // frappe.ui.form.on("Purchase Order", "refresh", function(frm) {
 //     cur_frm.set_query("supplier", function() {
