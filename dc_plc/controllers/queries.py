a9d4670b823faa31aff13ed387002e88268f1f4b
igrekus <upload234@mail.ru>
2019-03-12 18:05:28 +0300

    FIX: group indexes for consultant and developer filters queries

--- dc_plc/controllers/queries.py
+++ dc_plc/controllers/queries.py
@@ -5,14 +5,14 @@ from __future__ import unicode_literals
 import frappe
 
 
-# search for members of consultant group (GRP00000)
+# search for members of consultant group (GRP00001)
 def consultant_query(doctype, txt, searchfield, start, page_len, filters):
     
     return frappe.db.sql("""SELECT em.employee, em.employee_name
         FROM tabEmployee as em
         INNER JOIN tabDC_Employees_in_Group as gp
         ON em.employee = gp.link_employee
-        WHERE gp.parent = "GRP00000"
+        WHERE gp.parent = "GRP00001"
           AND em.status = 'Active'
           AND (em.name like %(search)s OR
                em.employee_name like %(search)s)
@@ -22,13 +22,13 @@ def consultant_query(doctype, txt, searchfield, start, page_len, filters):
                              'search': '%{}%'.format(txt)
                          })
 
-# search for members of the developer group (GRP00001)
+# search for members of the developer group (GRP00002)
 def developer_query(doctype, txt, searchfield, start, page_len, filters):
     return frappe.db.sql("""SELECT em.employee, em.employee_name
         FROM tabEmployee as em
         INNER JOIN tabDC_Employees_in_Group as gp
         ON em.employee = gp.link_employee
-        WHERE gp.parent = "GRP00001"
+        WHERE gp.parent = "GRP00002"
           AND status = 'Active'
           AND (em.name like %(search)s OR
                em.employee_name like %(search)s)
