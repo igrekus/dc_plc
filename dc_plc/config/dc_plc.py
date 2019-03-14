a1499ac1abd9f9ff052bcf6f1a2c0c38179055f8
igrekus <upload234@mail.ru>
2019-03-14 17:15:32 +0300

    Add module to the desktop in devenv

---
+++ dc_plc/config/dc_plc.py
@@ -0,0 +1,50 @@
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+    return [
+        {
+            "label": _("DC product tracking"),
+            "icon": "octicon octicon-briefcase",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "DC_PLC_Product_Summary",
+                    "label": _("Product summary"),
+                    "onboard": 1,
+                },
+                {
+                    "type": "doctype",
+                    "name": "DC_PLC_Package",
+                    "label": _("Package type"),
+                },
+                {
+                    "type": "doctype",
+                    "name": "DC_PLC_Product_Function",
+                    "label": _("Product function"),
+                },
+                {
+                    "type": "doctype",
+                    "name": "DC_PLC_Product_Type",
+                    "label": _("Product type"),
+                },
+                {
+                    "type": "doctype",
+                    "name": "DC_PLC_RND_Project",
+                    "label": _("R&D project"),
+                },
+            ]
+        },
+        {
+            "label": _("DC Staff"),
+            "icon": "octicon octicon-briefcase",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "DC_Employee_Group",
+                    "label": _("Employees group"),
+                },
+            ]
+        }
+    ]
