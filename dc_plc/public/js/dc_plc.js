ee3c9e508c4b4305f28f4c2ff95a2653509564ee
igrekus <upload234@mail.ru>
2019-03-19 18:41:17 +0300

    WIP: hook system fiddle

--- dc_plc/public/js/dc_plc.js
+++ dc_plc/public/js/dc_plc.js
@@ -1,9 +1,3 @@
-import { view_test } from "./views/productsummaryview.js";
+console.log(">>> DC PLC init <<<");
 
-// frappe.provide("dc_plc");
-
-console.log("DC PLC init");
-view_test();
-
-// dc_plc.echo = view_test;
-
+frappe.views.list_view["dc_list"] = new frappe.views.DCProductView();
--- dc_plc/public/js/views/productsummaryview.js
+++ dc_plc/public/js/views/productsummaryview.js
@@ -1,41 +1,37 @@
-export function view_test () {
-    console.log(">>> custom js <<");
-}
+frappe.provide("frappe.dc_plc");
 
+frappe.views.DCProductView = class DCProductView extends frappe.views.ListView {
 
-// frappe.provide('frappe.views');
+    get view_name() {
+        return 'DC Product View';
+    }
 
+	setup_defaults() {
+		super.setup_defaults();
+		this.page_title = __('Product:') + ' ' + this.page_title;
+		this.menu_items = this.report_menu_items();
 
-// frappe.views.ReportView = class ReportView extends frappe.views.ListView {
-//     // get view_name() {
-//     //     return 'Product View';
-//     // }
-//     //
-//     // setup_defaults() {
-//     //     super.setup_defaults();
-//     //     this.page_title = __('Product View:') + ' ' + this.page_title;
-//     //     this.menu_items = this.report_menu_items();
-//     //
-//     //     const route = frappe.get_route();
-//     //     if (route.length === 4) {
-//     //         this.report_name = route[3];
-//     //     }
-//     //
-//     //     this.add_totals_row = this.view_user_settings.add_totals_row || 0;
-//     //
-//     //     if (this.report_name) {
-//     //         return this.get_report_doc()
-//     //             .then(doc => {
-//     //                 this.report_doc = doc;
-//     //                 this.report_doc.json = JSON.parse(this.report_doc.json);
-//     //
-//     //                 this.filters = this.report_doc.json.filters;
-//     //                 this.order_by = this.report_doc.json.order_by;
-//     //                 this.add_totals_row = this.report_doc.json.add_totals_row;
-//     //                 this.page_title = this.report_name;
-//     //                 this.page_length = this.report_doc.json.page_length || 20;
-//     //                 this.order_by = this.report_doc.json.order_by || 'modified desc';
-//     //             });
-//     //     }
-//     // }
-// };
+		const route = frappe.get_route();
+		console.log(route);
+		// if (route.length === 4) {
+		// 	this.report_name = route[3];
+		// }
+        //
+		// this.add_totals_row = this.view_user_settings.add_totals_row || 0;
+        //
+		// if (this.report_name) {
+		// 	return this.get_report_doc()
+		// 		.then(doc => {
+		// 			this.report_doc = doc;
+		// 			this.report_doc.json = JSON.parse(this.report_doc.json);
+        //
+		// 			this.filters = this.report_doc.json.filters;
+		// 			this.order_by = this.report_doc.json.order_by;
+		// 			this.add_totals_row = this.report_doc.json.add_totals_row;
+		// 			this.page_title = this.report_name;
+		// 			this.page_length = this.report_doc.json.page_length || 20;
+		// 			this.order_by = this.report_doc.json.order_by || 'modified desc';
+		// 		});
+		// }
+	}
+};
