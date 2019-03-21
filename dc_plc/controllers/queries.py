258cd3eb29c7f949c80817a9e66988cf4f080df0
igrekus <upload234@mail.ru>
2019-03-21 15:47:27 +0300

    MISC: create report helper queries

--- dc_plc/controllers/queries.py
+++ dc_plc/controllers/queries.py
@@ -5,8 +5,17 @@ from __future__ import unicode_literals
 import frappe
 
 
-# search for members of consultant group (GRP00001)
 def consultant_query(doctype, txt, searchfield, start, page_len, filters):
+    """
+    Search members of consultant group (GRP00001)
+    :param doctype:
+    :param txt:
+    :param searchfield:
+    :param start:
+    :param page_len:
+    :param filters:
+    :return:
+    """
     
     return frappe.db.sql("""SELECT em.employee, em.employee_name
         FROM tabEmployee as em
@@ -22,8 +31,18 @@ def consultant_query(doctype, txt, searchfield, start, page_len, filters):
                              'search': '%{}%'.format(txt)
                          })
 
-# search for members of the developer group (GRP00002)
+
 def developer_query(doctype, txt, searchfield, start, page_len, filters):
+    """
+    Search for members of the developer group (GRP00002)
+    :param doctype:
+    :param txt:
+    :param searchfield:
+    :param start:
+    :param page_len:
+    :param filters:
+    :return:
+    """
     return frappe.db.sql("""SELECT em.employee, em.employee_name
         FROM tabEmployee as em
         INNER JOIN tabDC_Employees_in_Group as gp
@@ -38,3 +57,44 @@ def developer_query(doctype, txt, searchfield, start, page_len, filters):
                              'search': '%{}%'.format(txt)
                          })
 
+
+def developers_in_product():
+    return frappe.db.sql("""SELECT CONCAT(emp.last_name, " ", emp.first_name, " ", emp.middle_name) AS developers
+FROM `tabDC_PLC_Developers_in_Product` AS t
+INNER JOIN tabEmployee AS emp
+ON t.link_employee = emp.employee;
+""")
+
+
+def consultants_in_product():
+    return frappe.db.sql("""SELECT CONCAT(emp.last_name, " ", emp.first_name, " ", emp.middle_name) AS consultants
+    FROM `tabDC_PLC_Consultants_in_Product` AS t
+    INNER JOIN tabEmployee AS emp
+    ON t.link_employee = emp.employee;
+    """)
+
+
+def product_stats_full():
+    return frappe.db.sql("""SELECT
+  p.name as `id`
+     , p.ext_num
+     , type.title
+     , proj.title
+     , "stub"
+     , "stub"
+     , p.chip
+     , p.asm_board
+     , pak.title
+     , fun.title
+     , p.application
+     , p.description
+     , p.specs
+     , p.opcon
+     , p.analog
+     , p.report
+     , p.datasheet
+FROM `_1bd3e0294da19198`.tabDC_PLC_Product_Summary AS p
+INNER JOIN `_1bd3e0294da19198`.tabDC_PLC_Product_Type AS type,
+           `_1bd3e0294da19198`.tabDC_PLC_RND_Project AS proj,
+           `_1bd3e0294da19198`.tabDC_PLC_Package AS pak,
+           `_1bd3e0294da19198`.tabDC_PLC_Product_Function AS fun;""", as_list=1)
