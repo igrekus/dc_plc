effb42ee228f1b9f74e89bc0c419219c0a0cf290
igrekus <upload234@mail.ru>
2019-03-21 15:56:20 +0300

    FIX: rnd specialist report query

--- dc_plc/dc_plc/report/dc_product_rnd_specialist_stats/dc_product_rnd_specialist_stats.py
+++ dc_plc/dc_plc/report/dc_product_rnd_specialist_stats/dc_product_rnd_specialist_stats.py
@@ -23,24 +23,9 @@ def get_data():
 	     , p.ext_num
 	     , type.title
 	     , proj.title
-	     , "stub"
-	     , "stub"
-	     , p.chip
-	     , p.asm_board
-	     , pak.title
-	     , fun.title
-	     , p.application
-	     , p.description
-	     , p.specs
-	     , p.opcon
-	     , p.analog
-	     , p.report
-	     , p.datasheet
 	FROM `_1bd3e0294da19198`.tabDC_PLC_Product_Summary AS p
 	INNER JOIN `_1bd3e0294da19198`.tabDC_PLC_Product_Type AS type,
-	           `_1bd3e0294da19198`.tabDC_PLC_RND_Project AS proj,
-	           `_1bd3e0294da19198`.tabDC_PLC_Package AS pak,
-	           `_1bd3e0294da19198`.tabDC_PLC_Product_Function AS fun;""", as_list=1)
+	           `_1bd3e0294da19198`.tabDC_PLC_RND_Project AS proj;""", as_list=1)
 
 	return result
 
