fe0b214e3dca57b7f6ffc850c1cf9fc71e277827
igrekus <upload234@mail.ru>
2019-06-20 16:12:14 +0300

    FEAT(full stats): add relevance to full stats query

--- dc_plc/dc_plc/report/dc_product_stats/dc_product_stats.py
+++ dc_plc/dc_plc/report/dc_product_stats/dc_product_stats.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2013, igrekus and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals, division
 import frappe
 from frappe import _
 
 from dc_plc.custom.utils import add_product_summary_links, add_translation, add_completeness
-from dc_plc.controllers.stats_query import get_full_stats, get_developers_for_product, get_consultants_for_product
+from dc_plc.controllers.stats_query import get_full_stats, get_developers_for_product, get_consultants_for_product, get_full_stats_relevance
 
 
 def execute(filters=None):
@@ -19,6 +19,7 @@ def execute(filters=None):
 def get_columns():
 	return [
 		"ID:Link/DC_PLC_Product_Summary",
+		_("Relevance"),
 		_("Progress"),
 		_("Status"),
 		_("External number"),
@@ -46,14 +47,39 @@ def get_columns():
 def get_data(filters):
 	# TODO sanitize filters
 	# TODO refactor hardcoded indexes
+
+	relevance_values = {
+		0: 3,
+		1: 1,
+		2: 10,
+		3: 2,
+		4: 1,
+		5: 2,
+		6: 2
+	}
+
 	def add_devs_and_cons(row):
 		row[7] = cons.get(row[0], '').replace(',', '<br>')
 		row[8] = devs.get(row[0], '').replace(',', '<br>')
 		return row
 
+	def add_relevance(row):
+		done = 0
+		total = 21
+		flags = row[22:]
+		for index, flag in enumerate(flags):
+			if flag:
+				done += relevance_values[index]
+		percent = int((done/total) * 100)
+		return [row[0]] + [str(percent) + '%'] + row[1:]
+
 	devs = get_developers_for_product()
 	cons = get_consultants_for_product()
 	result = get_full_stats(filters)
 
-	return [add_product_summary_links(add_translation(add_completeness(add_devs_and_cons(row), range(len(row)))),
-										host=frappe.utils.get_url()) for row in result]
+	result = [add_devs_and_cons(row) for row in result]
+	result = [add_completeness(row, range(21)) for row in result]
+	result = [add_relevance(row) for row in result]
+	result = [add_product_summary_links(row, host=frappe.utils.get_url()) for row in result]
+
+	return result
