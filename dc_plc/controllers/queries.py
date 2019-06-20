d9ac6085c80aeb5863926efa05940adc2d435f57
igrekus <upload234@mail.ru>
2019-06-20 17:49:16 +0300

    FIX: remove dupe import

--- dc_plc/controllers/queries.py
+++ dc_plc/controllers/queries.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: GNU General Public License v3. See license.txt
 
-from __future__ import unicode_literals, division, unicode_literals
+from __future__ import unicode_literals, division
 
 from collections import defaultdict
 
