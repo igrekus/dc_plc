3ab8e23d8121df0dfbdb3ba77cf018877a8b6c51
igrekus <upload234@mail.ru>
2019-07-01 16:27:30 +0300

    FEAT(developer stats): add devs and cons columns

--- dc_plc/dc_plc/report/dc_product_developer_stats/dc_product_developer_stats.py
+++ dc_plc/dc_plc/report/dc_product_developer_stats/dc_product_developer_stats.py
@@ -6,7 +6,7 @@ import frappe
 from frappe import _
 
 from dc_plc.custom.utils import add_product_summary_links, add_completeness, add_query_relevance
-from dc_plc.controllers.stats_query import get_developer_stats
+from dc_plc.controllers.stats_query import get_developer_stats, get_developers_for_product, get_consultants_for_product
 
 
 def execute(filters=None):
@@ -22,6 +22,8 @@ def get_columns():
 		_("Relevance"),
 		_("Progress"),
 		_("RnD Title"),
+		_("Consultants"),
+		_("Developers"),
 		_("Type"),
 		_("Model"),
 		_("Function"),
@@ -38,11 +40,20 @@ def get_columns():
 
 
 def get_data(filters):
+
+	def add_devs_and_cons(row):
+		row[2] = cons.get(row[0], '').replace(',', '<br>')
+		row[3] = devs.get(row[0], '').replace(',', '<br>')
+		return row
+
 	host = frappe.utils.get_url()
+	devs = get_developers_for_product()
+	cons = get_consultants_for_product()
 
 	has_perms = 'DC_PLC_Developer' in frappe.get_roles(frappe.session.user)
 
 	res = get_developer_stats(filters)
+	res = [add_devs_and_cons(row) for row in res]
 	res = [add_completeness(row, range(2, 12)) for row in res]
 	res = [add_query_relevance(row, has_perms) for row in res]
 	res = [add_product_summary_links(row, host=host) for row in res]
