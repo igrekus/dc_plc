16c3ca6de9efbc0bf48a7e6bcd3c135f01e6547f
igrekus <upload234@mail.ru>
2019-07-03 15:09:33 +0300

    FIX(style): use tabs

--- dc_plc/dc_plc/report/dc_product_mmic_dept_head_stats/dc_product_mmic_dept_head_stats.js
+++ dc_plc/dc_plc/report/dc_product_mmic_dept_head_stats/dc_product_mmic_dept_head_stats.js
@@ -10,14 +10,14 @@ let check_handle = (o) => {
 		method: "dc_plc.dc_plc.doctype.dc_plc_product_summary.dc_plc_product_summary.set_dept_head_relevant",
 		args: {
 			name: name,
-            relevant: relevant ? 1 : 0,
-        },
-        callback: r => {
-            let {date, check} = r.message;
-            $('.rel_label_' + name)[0].innerHTML = "&nbsp;&nbsp;" + date;
-            o.checked = !!check;
-        }
-    });
+			relevant: relevant ? 1 : 0,
+		},
+		callback: r => {
+			let {date, check} = r.message;
+			$('.rel_label_' + name)[0].innerHTML = "&nbsp;&nbsp;" + date;
+			o.checked = !!check;
+		}
+	});
 };
 
 // let higlight_column = (col_num) => {
@@ -26,29 +26,29 @@ let check_handle = (o) => {
 // };
 
 frappe.query_reports["DC Product MMIC Dept Head Stats"] = {
-    filters: [],
-    // formatter: (value, row, column, row_data, format) => {
-    //     let new_value = '';
-    //
-    //     console.log(column);
-    //
-    //     if (!value) {
-    //         return new_value;
-    //     }
-    //     if (column.id === __('Relevance')) {
-    //         return value;
-    //     }
-    //
-    //     new_value = `<a href="${window.location.host}/desk#Form/DC_PLC_Product_Summary/${row_data.ID}">${value}</a>`;
-    //
-    //     return new_value;
-    // },
-    // onload: report => {
-    //     // TODO hack, use API to highlight cols
-    //     setTimeout(() => {
-    //         higlight_column(4);
-    //         higlight_column(6);
-    //         higlight_column(7);
-    //     }, 1500);
-    // }
+	filters: [],
+	// formatter: (value, row, column, row_data, format) => {
+	//     let new_value = '';
+	//
+	//     console.log(column);
+	//
+	//     if (!value) {
+	//         return new_value;
+	//     }
+	//     if (column.id === __('Relevance')) {
+	//         return value;
+	//     }
+	//
+	//     new_value = `<a href="${window.location.host}/desk#Form/DC_PLC_Product_Summary/${row_data.ID}">${value}</a>`;
+	//
+	//     return new_value;
+	// },
+	// onload: report => {
+	//     // TODO hack, use API to highlight cols
+	//     setTimeout(() => {
+	//         higlight_column(4);
+	//         higlight_column(6);
+	//         higlight_column(7);
+	//     }, 1500);
+	// }
 };
