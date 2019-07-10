7265d864dda986ebea590915e926ef40c06b1174
igrekus <upload234@mail.ru>
2019-07-10 11:47:04 +0300

    REF(query_report formatter): move formatter to a module

--- dc_plc/public/js/dc_plc.js
+++ dc_plc/public/js/dc_plc.js
@@ -1,24 +1,9 @@
 (function () {
 
-    frappe.provide('frappe.dc_plc');
-    // frappe.dc_plc.product_link_formatter = frappe.require('assets/dc_plc/js/utils/formatter.js').product_link_formatter;
-
-    frappe.dc_plc.product_link_formatter = (value, row, column, row_data, format) => {
-        if (!value) {
-            value = '-';
-        }
-
-        if (column.id === __('Relevance')) {
-            let id = row[1].content;
-            let [date, check, perms] = value.split(';');
-            let check_str = parseInt(check) ? 'checked' : '';
-            if (parseInt(perms)) {
-                return `<input type="checkbox" id="${id}" onchange="check_handle(this)" ${check_str}/><label style="vertical-align: top; padding-top: 3px" class="rel_label_${id}" for="${id}">&nbsp&nbsp${date}</label>`
-            }
-            value = date;
-        }
-        return `<a href="http://${window.location.host}/desk#Form/DC_PLC_Product_Summary/${row_data.ID}">${value}</a>`;
-    };
+    frappe.provide('frappe.dc_plc.utils');
+    frappe.require('assets/dc_plc/js/utils/formatter.js', () => {
+        frappe.dc_plc.utils.product_link_formatter = product_link_formatter;
+    });
 
     console.log('>>> DC PLC init finished <<<');
 })();
