e2c6ee0c24e9fc55db65d661f66c028437884654
igrekus <upload234@mail.ru>
2019-07-10 16:07:37 +0300

    FIX(style): close semicolon in style property

--- dc_plc/public/js/utils/formatter.js
+++ dc_plc/public/js/utils/formatter.js
@@ -8,7 +8,7 @@ let product_link_formatter = (value, row, column, row_data, format) => {
         let [date, check, perms] = value.split(';');
         let check_str = parseInt(check) ? 'checked' : '';
         if (parseInt(perms)) {
-            return `<input type="checkbox" id="${id}" onchange="check_handle(this)" ${check_str}/><label style="vertical-align: top; padding-top: 3px" class="rel_label_${id}" for="${id}">&nbsp&nbsp${date}</label>`
+            return `<input type="checkbox" id="${id}" onchange="check_handle(this)" ${check_str}/><label style="vertical-align: top; padding-top: 3px;" class="rel_label_${id}" for="${id}">&nbsp&nbsp${date}</label>`
         }
         value = date;
     }
