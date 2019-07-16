23dd0d3a130d3b1add6f7bab6f088abe5f47d653
igrekus <upload234@mail.ru>
2019-07-16 18:32:08 +0300

    REF(handlers): remove unnecessary variable

--- dc_plc/public/js/utils/handler.js
+++ dc_plc/public/js/utils/handler.js
@@ -1,17 +1,15 @@
 let template_handler = (method, checkbox) => {
     let {checked, id} = checkbox;
-    let relevant = checked;
-    let name = id;
     frappe.call({
         method: "dc_plc.dc_plc.doctype.dc_plc_product_summary.dc_plc_product_summary." + method,
         args: {
-            name: name,
-            relevant: relevant ? 1 : 0,
+            name: id,
+            relevant: checked ? 1 : 0,
         },
         callback: r => {
             let {date, check} = r.message;
-            $('.rel_label_' + name)[0].innerHTML = "&nbsp;&nbsp;" + date;
-            o.checked = !!check;
+            $('.rel_label_' + id)[0].innerHTML = "&nbsp;&nbsp;" + date;
+            checkbox.checked = !!check;
         }
     });
 };
