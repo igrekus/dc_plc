30e5e1479f821619786be3c70bb549dadf865bbd
igrekus <upload234@mail.ru>
2019-08-06 18:51:27 +0300

    WIP: implement cart and export preview

--- dc_plc/public/js/export_tool/ToolRoot.vue
+++ dc_plc/public/js/export_tool/ToolRoot.vue
@@ -1,45 +1,56 @@
 <template>
 	<div>
-		<el-table
-				ref="exportTable"
-				v-bind:data="productsToExport"
-				style="width: 100%">
-			<el-table-column property="number" label="№" width="40"></el-table-column>
-			<el-table-column property="ext_num" label="Внешний номер" width="200"></el-table-column>
-			<el-table-column property="int_num" label="Внутрений нормер" width="200"></el-table-column>
-			<el-table-column property="rnd_proj" label="ОКР" width="300"></el-table-column>
-			<el-table-column align="center" width="80">
-				<template slot-scope="scope">
-					<el-button
-							size="mini"
-							type="danger"
-							icon="el-icon-delete"
-							plain
-							@click="handleDelete(scope.$index, scope.row)">
-					</el-button>
-				</template>
-			</el-table-column>
-		</el-table>
-		<div style="margin-top: 20px">
-			<el-autocomplete
-					placeholder="Поиск..."
-					v-model="state"
-					popper-class="product-autocomplete"
-					v-bind:fetch-suggestions="querySearchAsync"
-					v-on:select="handleAutocompleteSelect"
-					v-bind:trigger-on-focus="false">
-				<template slot-scope="{ item }">
-					<div class="suggestion-item">
-						<div class="ext_num">Внеш.: {{ item.ext_num }}</div>
-						<div class="int_num">Внут.: {{ item.int_num }}</div>
-					</div>
-				</template>
-			</el-autocomplete>
-		</div>
+		<el-row>
+			<el-table
+					ref="exportTable"
+					v-bind:data="productsToExport"
+					style="width: 100%">
+				<div slot="empty">
+					Нет данных
+				</div>
+				<el-table-column property="number" label="№" width="40"></el-table-column>
+				<el-table-column property="ext_num" label="Внешний номер" width="200"></el-table-column>
+				<el-table-column property="int_num" label="Внутрений нормер" width="200"></el-table-column>
+				<el-table-column property="rnd_proj" label="ОКР" width="300"></el-table-column>
+				<el-table-column align="center" width="80">
+					<template slot-scope="scope">
+						<el-button
+								size="mini"
+								type="danger"
+								icon="el-icon-delete"
+								plain
+								v-on:click="handleDelete(scope.$index, scope.row)">
+						</el-button>
+					</template>
+				</el-table-column>
+			</el-table>
+			<div style="margin-top: 20px">
+				<el-autocomplete
+						placeholder="Поиск..."
+						v-model="state"
+						popper-class="product-autocomplete"
+						v-bind:fetch-suggestions="querySearchAsync"
+						v-on:select="handleAutocompleteSelect"
+						v-bind:trigger-on-focus="false">
+					<template slot-scope="{ item }">
+						<div class="suggestion-item">
+							<div class="ext_num">Внеш.: <span class="item-text">{{ item.ext_num }}</span></div>
+							<div class="int_num">Внут.: <span class="item-text">{{ item.int_num }}</span></div>
+						</div>
+					</template>
+				</el-autocomplete>
+			</div>
+		</el-row>
+		<!--<el-divider/>-->
+		<el-row>
+			<export-widget v-bind:productIds="product_ids"></export-widget>
+		</el-row>
 	</div>
 </template>
 
 <script>
+	import ExportWidget from './ExportWidget.vue'
+
 	export default {
 		name: "ToolRoot",
 		data() {
@@ -68,15 +79,15 @@
 			}
 		},
 		methods: {
-			toggleSelection(rows) {
-				if (rows) {
-					rows.forEach(row => {
-						this.$refs.exportTable.toggleRowSelection(row);
-					});
-				} else {
-					this.$refs.exportTable.clearSelection();
-				}
-			},
+			// toggleSelection(rows) {
+			// 	if (rows) {
+			// 		rows.forEach(row => {
+			// 			this.$refs.exportTable.toggleRowSelection(row);
+			// 		});
+			// 	} else {
+			// 		this.$refs.exportTable.clearSelection();
+			// 	}
+			// },
 			querySearchAsync(queryString, cb) {
 				frappe.call({
 					method: "dc_plc.controllers.export_tool.export_product_search",
@@ -103,30 +114,35 @@
 				this.product_ids.splice(row_index, 1);
 			}
 		},
+		components: {
+			ExportWidget,
+		}
 	}
 </script>
 
 <style>
-	.product-autocomplete {
+	.product-autocomplete li {
+		line-height: normal;
+		padding: 7px;
+	}
 
-		li {
-			line-height: normal;
-			padding: 7px;
+	li {
+		border-bottom-width: 1px;
+		border-bottom-style: solid;
+		border-bottom-color: #e1e1e1;
+	}
 
-			.suggestion-item {
-				border-bottom-width: 1px;
-				border-bottom-style: dashed;
-			}
+	li .ext_num {
+		width: 95%;
+		font-size: 14px;
+	}
 
-			.ext_num {
-				text-overflow: ellipsis;
-				overflow: hidden;
-			}
+	li .int_num {
+		width: 95%;
+		font-size: 14px;
+	}
 
-			.int_num {
-				font-size: 12px;
-				color: #b4b4b4;
-			}
-		}
+	li .item-text {
+		float: right
 	}
 </style>
