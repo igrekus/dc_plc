62d284b42357d10ac259c9d25f4e4e13f68f0331
igrekus <upload234@mail.ru>
2019-10-16 18:24:04 +0300

    FEAT: implement misc docs uploader

--- dc_plc/controllers/role_file_uploader.py
+++ dc_plc/controllers/role_file_uploader.py
@@ -3,6 +3,7 @@ import frappe
 import os
 import shutil
 
+from dc_plc.dc_documents.doctype.dc_doc_misc_meta.dc_doc_misc_meta import DC_Doc_Misc_Meta
 from frappe.core.doctype.file.file import File
 from dc_plc.dc_documents.doctype.dc_doc_dev_report_meta.dc_doc_dev_report_meta import DC_Doc_Dev_Report_Meta
 from dc_plc.dc_plc.doctype.dc_plc_product_summary.dc_plc_product_summary import DC_PLC_Product_Summary
@@ -114,6 +115,58 @@ LIMIT 10;""", {
 
 
 @frappe.whitelist()
+def search_existing_misc(query):
+	"""
+	Search for existing developer reports
+	:param query: str -- text search query
+	:return: list -- list of {'value': '', 'label': ''} entries
+	"""
+
+	db_name = frappe.conf.get("db_name")
+
+	res = frappe.db.sql(f"""
+SELECT DISTINCT 
+	`m`.`name`
+	, `m`.`title` AS `meta_title`
+	, `m`.`note` AS `note`
+	, `m`.`attached_file` AS `url`
+FROM
+	`{db_name}`.tabDC_Doc_Misc_Meta AS `m`
+INNER JOIN
+	`{db_name}`.tabDC_Doc_Document_Subtype AS `s` ON `m`.`link_subtype` = `s`.`name`
+INNER JOIN
+	`{db_name}`.tabDC_Doc_Misc_in_Misc_List AS `l` ON `l`.`link_misc_meta` = `m`.`name`
+INNER JOIN
+	`{db_name}`.tabDC_PLC_Product_Summary AS `p` ON `p`.`name` = `l`.`parent`
+WHERE
+	`s`.`name` = 'DST004'
+AND
+	(
+	`m`.`title` LIKE %(search)s
+	OR
+	`p`.`int_num` LIKE %(search)s
+	OR
+	`p`.`ext_num` LIKE %(search)s
+	OR
+	DATE(`m`.`creation`) < %(date)s
+	) 
+LIMIT 10;""", {
+		'search': '%{}%'.format(query),
+		'date': '{}'.format(query),
+	})
+
+	return [
+		{
+			'label': d[0],
+			'value': d[1],
+			'note': d[2],
+			'file_url': d[3].replace('\n', '<br>')
+		}
+		for d in res
+	]
+
+
+@frappe.whitelist()
 def upload_file(*args, **kwargs):
 	file_type = kwargs['fileType']
 
@@ -169,7 +222,7 @@ def add_existing_datasheet(prod_id, datasheet):
 def add_new_datasheet(prod_id, datasheet, temp_file):
 	file_name = temp_file.split('/')[-1]
 	target_file = f'{datasheet["file_url"]}{file_name}'
-	stored_url = target_file[20:]
+	stored_url = target_file[20:]   # TODO make less hacky
 
 	try:
 		shutil.move(temp_file, target_file, copy_function=shutil.copy)
@@ -266,5 +319,71 @@ def add_new_dev_report(prod_id, dev_report, temp_file):
 	new_file.insert()
 
 	add_existing_dev_report(prod_id, dev_report={'label': new_meta.name})
+
+	return True
+
+
+@frappe.whitelist()
+def add_misc(prod_id, upload, temp_file):
+	ds = frappe.parse_json(upload)
+
+	if ds['label']:
+		add_existing_misc(prod_id, ds)
+	else:
+		add_new_misc(prod_id, ds, temp_file)
+
+	return 'success'
+
+
+def add_existing_misc(prod_id, misc):
+	doc: DC_PLC_Product_Summary = frappe.get_doc('DC_PLC_Product_Summary', prod_id)
+	meta: DC_Doc_Misc_Meta = frappe.get_doc('DC_Doc_Misc_Meta', misc['label'])
+	doc_subype = frappe.get_doc('DC_Doc_Document_Subtype', meta.link_subtype)
+	doc_type = frappe.get_doc('DC_Doc_Document_Type', doc_subype.link_doc_type)
+
+	doc.append('tab_misc', {
+		'link_misc_meta': meta.name,
+		'doc_type': doc_type.title,
+		'doc_subtype': doc_subype.title,
+	})
+	doc.save()
+
+	return True
+
+
+def add_new_misc(prod_id, misc_file, temp_file):
+	file_name = temp_file.split('/')[-1]
+	target_file = f'{misc_file["file_url"]}{file_name}'
+	stored_url = target_file[20:]
+
+	try:
+		shutil.move(temp_file, target_file, copy_function=shutil.copy)
+	except PermissionError:
+		pass
+	file_info = os.stat(target_file)
+
+	new_misc: DC_Doc_Misc_Meta = frappe.get_doc({
+		'doctype': 'DC_Doc_Misc_Meta',
+		'title': misc_file['value'],
+		'link_subtype': 'DST004',
+		'attached_file': stored_url,
+		'note': misc_file['note']
+	})
+	new_misc.insert()
+
+	new_file: File = frappe.get_doc({
+		'doctype': 'File',
+		'file_name': misc_file['value'],
+		'is_private': 0,
+		'file_size': file_info.st_size,
+		'file_url': stored_url,
+		'folder': 'Home/Misc_Files',
+		'attached_to_doctype': 'DC_Doc_Misc_Meta',
+		'attached_to_name': new_misc.name,
+		'attached_to_field': 'attached_file',
+	})
+	new_file.insert()
+
+	add_existing_misc(prod_id, misc={'label': new_misc.name})
 
 	return True
