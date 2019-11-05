36015e0a54b09edef9c2c5e6ede10a35a52dc920
igrekus <upload234@mail.ru>
2019-11-05 12:07:43 +0300

    MISC: remove unused code

--- dc_plc/dc_plc/page/dc_product_dashboard/dc_product_dashboard.js
+++ dc_plc/dc_plc/page/dc_product_dashboard/dc_product_dashboard.js
@@ -12,33 +12,10 @@ frappe.pages["dc_product_dashboard"].on_page_load = (wrapper) => {
 
 	frappe.dc_plc.show_dashboard(this.page);
 
-	// me.page.main.on("click", ".activity-message", function() {
-	// 	var link_doctype = $(this).attr("data-link-doctype"),
-	// 		link_name = $(this).attr("data-link-name"),
-	// 		doctype = $(this).attr("data-doctype"),
-	// 		docname = $(this).attr("data-docname");
-	//
-	// 	if (doctype && docname) {
-	// 		if (link_doctype && link_name) {
-	// 			frappe.route_options = {
-	// 				scroll_to: { "doctype": doctype, "name": docname }
-	// 			}
-	// 		}
-	//
-	// 		frappe.set_route(["Form", link_doctype || doctype, link_name || docname]);
-	// 	}
-	// });
-
 	// Build Report Button
 	this.page.add_menu_item(__("Menu item 1"), () => {
 		console.log("menu item 1");
-		// frappe.route_options = {
-		// 	"user": frappe.session.user
-		// };
-		// frappe.set_route("List", "Feed", "Report");
 	}, 'fa fa-th');
-	// this.page.add_button("BOTON", () => console.log("BOTON"), "fa fa-th", false);
-	// this.page.add_inner_button("BOTON", () => console.log("BOTON"), "test", type="default");
 };
 
 frappe.pages['dc_product_dashboard'].on_page_show = () => {
