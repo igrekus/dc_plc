8410b045ff8acb2e723209e3c8c77996da8b3a9c
igrekus <upload234@mail.ru>
2019-11-11 16:10:21 +0300

    FEAT: highlight rnd specialist checked rows in green

--- dc_plc/dc_plc/report/dc_product_rnd_specialist_stats/dc_product_rnd_specialist_stats.js
+++ dc_plc/dc_plc/report/dc_product_rnd_specialist_stats/dc_product_rnd_specialist_stats.js
@@ -7,9 +7,17 @@ frappe.query_reports["DC Product RND Specialist Stats"] = {
 	formatter: frappe.dc_plc.utils.formatters.rnd_spec_formatter,
 	after_datatable_render: table_instance => {
 		let highlight_cols = [5];
-		highlight_cols.forEach(col => {
-			table_instance.style.setStyle(`.dt-cell--col-${col}`, {backgroundColor: 'rgba(255, 252, 29, 0.27);'})
-		});
+		let data = table_instance.datamanager.data;
+
+		for (let row = 0; row < data.length; ++row) {
+			const [date, check, perms] = data[row][__('Relevance')].split(';');
+			const is_checked = !!parseInt(check);
+			highlight_cols.forEach(col => {
+				let bgCol = is_checked ? 'rgba(37,220,2,0.2);' : 'rgba(255, 252, 29, 0.27);';
+				table_instance.style.setStyle(`.dt-cell--${col}-${row}`, {backgroundColor: `${bgCol}`});
+			});
+		}
+		table_instance.style.setStyle(`.dt-scrollable`, {height: '550px;'});
 	},
 	onload: report => {
 		report.export_products = [];
