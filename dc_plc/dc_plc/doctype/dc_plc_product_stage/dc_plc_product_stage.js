bd7891f3ca36096192bbfb6ae9812237a606b7e5
igrekus <upload234@mail.ru>
2019-12-04 17:11:28 +0300

    WIP: add form boilerplate

--- dc_plc/dc_plc/doctype/dc_plc_product_stage/dc_plc_product_stage.js
+++ dc_plc/dc_plc/doctype/dc_plc_product_stage/dc_plc_product_stage.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2019, igrekus and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('DC_PLC_Product_Stage', {
-	// refresh: function(frm) {
+let value_or_none = frappe.dc_plc.utils.value_or_none;
+let set_field_title = frappe.dc_plc.utils.ui.set_field_title;
+let set_info_field = frappe.dc_plc.utils.ui.set_info_field;
+let render_info_field = frappe.dc_plc.utils.ui.render_info_field;
+let render_field_title = frappe.dc_plc.utils.ui.render_field_title;
 
-	// }
+frappe.ui.form.on('DC_PLC_Product_Stage', {
+	refresh: function(frm) {
+		set_field_title(frm, 'link_milestone');
+	},
+
+	link_milestone: frm => {
+		let field = 'link_milestone';
+		frappe.db.get_doc(frm.fields_dict[field].df.options, frm.fields_dict[field].value).then(result => {
+			render_field_title(frm, field, value_or_none(result.title));
+		});
+	},
+
 });
