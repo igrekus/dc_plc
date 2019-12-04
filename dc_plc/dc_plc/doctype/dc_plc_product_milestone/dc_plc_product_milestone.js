dae03796d445fb6cc5bf8fd3b9e86a42d0848d05
igrekus <upload234@mail.ru>
2019-12-04 16:59:48 +0300

    WIP: add new DocTypes

---
+++ dc_plc/dc_plc/doctype/dc_plc_product_milestone/dc_plc_product_milestone.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, igrekus and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('DC_PLC_Product_Milestone', {
+	// refresh: function(frm) {
+
+	// }
+});
