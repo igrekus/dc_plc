bcf07474b2a41b373d0fc8b75a3602127100237c
igrekus <upload234@mail.ru>
2019-12-24 18:55:42 +0300

    FIX: remove status report doctype document from the system

--- dc_plc/dc_plc/report/dc_product_filter_status/dc_product_filter_status.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016, igrekus and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["DC Product Filter Status"] = {
-	"filters": [
-	],
-	formatter: frappe.dc_plc.utils.formatters.status_link_formatter,
-};
